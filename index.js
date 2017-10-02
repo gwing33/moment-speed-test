2ca5f26990d4a3793314b2810b887e16950355ca
Gerald Leenerts <gerald.leenerts@gmail.com>
2017-10-02 11:19:00 -0700

    Set a loop constant

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const moment = require('moment');
 const momentTimezone = require('moment-timezone');
 
+const loops = 10000;
+
 function timeWrap(name, fn) {
   console.time(name);
   fn();
@@ -8,25 +10,25 @@ function timeWrap(name, fn) {
 }
 
 timeWrap('moment(dateStr)', function () {
-    for(let i = 0; i < 10000; i++) {
+    for(let i = 0; i < loops; i++) {
         moment('2017-10-02 16:39:45');
     }
 });
 
 timeWrap('moment(new Date(dateStr))', function () {
-    for(let i = 0; i < 10000; i++) {
+    for(let i = 0; i < loops; i++) {
         moment('2017-10-02 16:39:45');
     }
 });
 
 timeWrap('moment.tz(dateStr, timezone)', function () {
-    for(i = 0; i < 10000; i++) {
+    for(i = 0; i < loops; i++) {
         momentTimezone.tz('2017-10-02 16:39:45', 'America/Los_Angeles');
     }
 });
 
 timeWrap('moment.tz(new Date(dateStr), timezone)', function () {
-    for(i = 0; i < 10000; i++) {
+    for(i = 0; i < loops; i++) {
         momentTimezone.tz(new Date('2017-10-02 16:39:45'), 'America/Los_Angeles');
     }
 });
